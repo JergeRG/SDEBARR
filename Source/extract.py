15f331ec15751aec8b9410b1f01761dccaf80b2c
JergeRG <jorgerupiita@gmail.com>
2020-10-12 12:12:36 -0500

    Se agrego linea para eliminar los subAspectos

--- Source/extract.py
+++ Source/extract.py
@@ -13,18 +13,44 @@ def extractData(folder):
     lstFile = nfile.split('.')
     
     lstReview = [['Id', 'Sentences', 'Aspects', 'Polarities']]
+    totalReviews, totalSentences  = 0, 0
+    dictAspects, dictPolarities = {}, {}
     for review in tree.findall('Review'):
+        totalReviews = totalReviews + 1 
         rid = review.get('rid')
-        lstSentence = []
-        lstAspect = []
-        lstPolarity = []
+        lstSentence, lstAspect, lstPolarity = [], [], []
         for sentence in review.find('sentences').findall('sentence'):
-            lstSentence.append(sentence.find('text').text)
+            totalSentences = totalSentences + 1
+            lstSentence.append(sentence.find('text').text.replace(',' , '_'))
         for opinion in review.find('Opinions').findall('Opinion'):
-            lstAspect.append(opinion.get('category'))
-            lstPolarity.append(opinion.get('polarity'))
+            aspect = opinion.get('category').split('#')[0]
+            lstAspect.append(aspect)
+            if aspect in dictAspects:
+                dictAspects[aspect] = dictAspects[aspect] + 1 
+            else:
+                dictAspects[aspect] = 0
+            
+            polarity = opinion.get('polarity')
+            if polarity in dictPolarities:
+                dictPolarities[polarity] = dictPolarities[polarity] + 1 
+            else:
+                dictPolarities[polarity] = 0
+            lstPolarity.append(polarity)
+            
         lstReview.append([rid, lstSentence, lstAspect, lstPolarity])
         
-        with open(npath + '/' + lstFile[0] + '.csv', 'w') as file:
-            writer = csv.writer(file)
-            writer.writerows(lstReview)
+    with open(npath + '/' + lstFile[0] + '.csv', 'w') as file:
+        writer = csv.writer(file)
+        writer.writerows(lstReview)
+                
+    with open(npath + '/Resume_' + lstFile[0] + '.txt', 'w') as file:
+        file.write('*'*10 + 'Resumen de datos leídos' +'*'*10 + '\n')
+        file.write('\t Total de reseñas leídas:\t' + str(totalReviews) + '\n')
+        file.write('\t Total de parrafos leídas:\t' + str(totalSentences) + '\n')
+        file.write('\t Total de aspectos hallados:' + '\n')
+        for (key, value) in dictAspects.items():
+            file.write('\t\t' + str(key) + ':\t' + str(value) + '\n')
+        file.write('\t Total de polaridades halladas:' + '\n')   
+        for (key, value) in dictPolarities.items():
+            file.write('\t\t' + str(key) + ':\t' + str(value) + '\n')
+            
